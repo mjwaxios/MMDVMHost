747eb84cb66cfb293f1026d843adbb77716c9896
mwyrick <mjw@axiosengineering.com>
2017-06-07 15:24:54 -0400

    Moved default ini to local dir

--- MMDVMHost.cpp
+++ MMDVMHost.cpp
@@ -57,7 +57,7 @@
 #if defined(_WIN32) || defined(_WIN64)
 const char* DEFAULT_INI_FILE = "MMDVM.ini";
 #else
-const char* DEFAULT_INI_FILE = "/etc/MMDVM.ini";
+const char* DEFAULT_INI_FILE = "MMDVM.ini";
 #endif
 
 static bool m_killed = false;
